a7ea597c917ec96a7b4dd174901d589916c7cdab
Yanis <yanis.urbis@gmail.com>
2019-11-13 22:54:55 +0300

    Fix a bug

--- src/transport_tycoon/core.clj
+++ src/transport_tycoon/core.clj
@@ -41,25 +41,27 @@
                              :duration 5}
                     :start-time 10})
 
+(def queue-to-deliver [:a :b :b])
+
+(def system {:initial-queue queue-to-deliver
+             :current-time 0
+             :queues {:factory queue-to-deliver
+                      :port []
+                      :a []
+                      :b []}
+             :events []
+             :history []
+             :actors [car-1 car-2 ship-1]})
+
 (defn is-completed-event? [event system]
-  {:pre [(do (println "start ~")
-          (pprint (:start-time event))
-          (pprint (get-in event [:action :duration]))
-          true)]
-   :post [(do (println "end ~") true)]}
   (>= (:current-time system)
     (+ (:start-time event) (get-in event [:action :duration]))))
 
 (defn get-completed-events [system]
-  (println "events")
-  (pprint (:events system))
-  (spit "./test.edn" (:events system) )
-  (filter #(is-completed-event? % system)
-    (:events system)))
-
-(get-completed-events {:events [{:start-time 0 :action {:duration 5}}
-                                {:start-time 0 :action {:duration 1}}]
-                       :current-time 2})
+  (->>
+    (:events system)
+    (filter #(is-completed-event? % system))
+    (map #(assoc % :end-time (:current-time system)))))
 
 (defn delivery-in-process? [system]
     (not=
@@ -67,23 +69,27 @@
       (+ (count (get-in system [:queues :a]))
         (count (get-in system [:queues :b])))))
 
-(delivery-in-process? {:initial-queue [2 1]
-                       :queues {:a [1 2]}})
-
 (defn get-element-from-queue [system queue-name]
   (-> (get-in system [:queues queue-name])
       first))
 
-(get-element-from-queue {:queues {:a [1 2 3]}} :a)
+(defn remove-element-from-queue [system queue-name]
+  (update-in system [:queues queue-name] rest))
 
 (defn get-random-id []
   (rand-int 10000000))
 
-(get-random-id)
+(defn prn! [data text]
+  (println)
+  (pprint (str "START~" text))
+  (pprint data)
+  (println (str "END~" text))
+  (println))
 
 (defn get-next-car-action-type [system car]
   (let [payload (get-element-from-queue system :factory)
         actor-position (:position car)]
+        ;(prn! [(:queues system) payload actor-position] "action-type")
        (case [payload actor-position]
           [:a :factory] :factory->port
           [:b :factory] :factory->b
@@ -95,8 +101,6 @@
           [nil :b] :b->factory
           :wait)))
 
-(get-next-car-action-type {:queues {:factory [:a :b]}} {:position :factory})
-
 (defn get-next-ship-action-type [system ship]
   (let [payload (get-element-from-queue system :port)
         actor-position (:position ship)]
@@ -107,26 +111,33 @@
           :wait)))
 
 (defn get-next-action-type [system actor]
-  (print actor)
   (case (:type actor)
     :car (get-next-car-action-type system actor)
     :ship (get-next-ship-action-type system actor)))
 
-(get-next-car-action-type {:queues {:factory [:a :b]}} {:position :factory :type :car})
+(defn get-next-action [system actor]
+  (get-in actor [:actions
+                (get-next-action-type system actor)]))
 
-(defn get-payload [system actor]
+(defn get-payload-and-new-system [system actor]
  (case [(:type actor) (:position actor)]
-    [:car :factory] (get-element-from-queue system :factory)
-    [:ship :port] (get-element-from-queue system :port)
-    nil))
+    [:car :factory] {:payload (get-element-from-queue system :factory)
+                     :system (remove-element-from-queue system :factory)}
+    [:ship :port] {:payload (get-element-from-queue system :port)
+                   :system (remove-element-from-queue system :port)}
+                  {:payload nil
+                  :system system}))
 
-(defn get-next-event [system actor]
-  {:id (get-random-id)
-   :actor actor
-   :action (get-in actor [:actions
-                          (get-next-action-type system actor)])
-   :start-time (:current-time system)
-   :payload (get-payload system actor)})
+(defn add-event-for-actor [system actor]
+  (let [action (get-next-action system actor)
+        {:keys [system payload]} (get-payload-and-new-system system actor)
+        event {:id (get-random-id)
+               :actor actor
+               :action action
+               :start-time (:current-time system)
+               :payload payload
+               :end-time nil }]
+    (update system :events conj event)))
 
 (defn add-payload-to-queue [system queue-name payload]
  (update-in system [:queues queue-name] conj payload))
@@ -146,35 +157,35 @@
     actor))
 
 (defn update-actor [actor event]
-  (if (= (:id actor) (get-in actor [:actor :id]))
-    (update-position actor (get-in event [:action :to]))))
+  ;{:pre [(do (prn [actor event] "update-actor-start") true)]
+  ; :post [(do (prn % "update-actor-end") true)]}
+  (if (= (:id actor) (get-in event [:actor :id]))
+    (update-position actor (get-in event [:action :to]))
+    actor))
 
 (defn update-actors [system event]
   (assoc system :actors (map #(update-actor % event) (:actors system))))
 
 (defn process-completed-events [system]
-  (pprint "Here")
-  (pprint (get-completed-events system))
-  (pprint "One")
-  (pprint (reduce
-            (fn [x y] x)
-            system
-            (get-completed-events system)))
   (reduce
-    identity
+    (fn [system event]
+      (-> system
+          (update-queues event)
+          (update-actors event)))
     system
     (get-completed-events system)))
 
 (defn generate-new-events [system]
-  (update system :events
-    into (map #(get-next-event system %) (:actors system))))
+  (reduce (fn [system actor]
+            ;(prn actor "actor")
+            (add-event-for-actor system actor))
+           system
+           (:actors system)))
 
 (defn remove-completed-events [system]
   (merge system
-    {:events (remove #(is-completed-event? % system)
-                     (:events system))}))
-     ;:history (merge (get-completed-events system) (:history system))}))
-
+    {:events (remove #(is-completed-event? % system) (:events system))
+     :history (into (:history system) (get-completed-events system))}))
 
 (defn update-time [system]
   (update system :current-time inc))
@@ -186,32 +197,43 @@
     remove-completed-events
     update-time))
 
-(def queue-to-deliver [:a])
-
-(def system {:initial-queue queue-to-deliver
-             :current-time 0
-             :queues {:factory queue-to-deliver
-                      :port []
-                      :a []
-                      :b []}
-             :events []
-             :history []
-             :actors [car-1 car-2 ship-1]})
-
 ;(remove-completed-events system)
 ;
-(->> system
-  process-completed-events
-  generate-new-events
-  ;remove-completed-events
-  update-time
-  process-completed-events
-  ;generate-new-events
-  ;remove-completed-events
-  update-time)
-
 ;(->> system
-;  (iterate tick)
-;  ;(drop-while (delivery-in-process? queue-to-deliver))
-;  (take 2))
+;  process-completed-events
+;  generate-new-events
+;  ;remove-completed-events
+;  update-time
+;  process-completed-events
+;  ;generate-new-events
+;  ;remove-completed-events
+;  update-time)
 
+(defn print-event [event]
+  (pprint [(:actor-id event) (:start-time event) (get-in event [:action :type]) (:payload event)])
+  ;(pprint event)
+  ;(println "-----------------------------------")
+  (println)
+  event)
+
+(defn pprint-system! [system]
+  (->>
+    (into (:events system) (:history system))
+    (remove #(= :wait (get-in % [:action :type])))
+    (map #(assoc % :actor-id (get-in % [:actor :id])))
+    (map #(assoc % :action-type (get-in % [:action :type])))
+    (map #(dissoc % :actor))
+    (sort-by :start-time)
+    (sort-by :actor-id)
+    (map #(select-keys % [:actor-id :start-time :end-time :action-type :payload]))
+    (reduce conj [])
+    print-table)
+  (println (:current-time system))
+  (println "-----------------------------"))
+
+(->> system
+  (iterate tick)
+  (drop-while delivery-in-process?)
+  (take 1)
+  (map pprint-system!)
+  doall)
